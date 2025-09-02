5a2c180e576e889473bea9755494904b0e0b63f6
pdlsagar677 <sagarpoudel677@gmail.com>
2025-09-02 12:31:06 +0545

    google maps and ride data completed and google api keys not verified

--- server/app.js
+++ server/app.js
@@ -7,6 +7,10 @@ const cookieParser = require('cookie-parser');
 const connectToDb = require('./db/db');
 const userRoutes = require('./src/routes/user-route');
 const captainRoutes = require('./src/routes/captain-route')
+const mapsRoutes = require('./src/routes/maps-route')
+const rideRoutes = require('./src/routes/ride-route')
+
+
 connectToDb();
 
 app.use(cors());
@@ -19,7 +23,8 @@ app.use(cookieParser());
 
 app.use('/api/users', userRoutes);
 app.use('/api/captain', captainRoutes);
-
+app.use('/maps', mapsRoutes);
+app.use('/rides', rideRoutes);
 
 
 
