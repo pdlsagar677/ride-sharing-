a0435ef17071dac7376829b641aad7b12a50048a
pdlsagar677 <sagarpoudel677@gmail.com>
2025-09-02 15:46:58 +0545

    react initalization and socket correction in serverjs

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 const http = require('http');
 const app = require('./app');
+const { initializeSocket } = require('./socket/socket');
 const port = process.env.PORT ;
 
 const server = http.createServer(app);
-
+initializeSocket(server);
 
 server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
